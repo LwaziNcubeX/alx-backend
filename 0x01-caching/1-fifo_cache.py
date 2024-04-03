7d2892648f3888823fd26cf60435e7545badec65
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-03 21:29:14 +0200

    change and to or in the if statement

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -23,7 +23,7 @@ class FIFOCache(BaseCaching):
         :param item:
         :return:
         """
-        if key is not None and item is not None:
+        if key is not None or item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 removed_key = self.cache_order.pop(0)
                 del self.cache_data[removed_key]
