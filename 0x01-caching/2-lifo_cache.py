c8edcbeda0e9ce68d858606765c2e9be258b9b84
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-03 21:59:14 +0200

    A class LIFO Cache that inherits from BaseCaching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A class LIFO Cache that inherits from BaseCaching
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    A class that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initialize the LIFOCache instance
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item to the LIFO cache
+        :param key:
+        :param item:
+        :return:
+        """
+
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded_key = next(reversed(self.cache_data))
+            del self.cache_data[discarded_key]
+            print("DISCARD:", discarded_key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item from the LIFO cache
+        :param key:
+        :return:
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
