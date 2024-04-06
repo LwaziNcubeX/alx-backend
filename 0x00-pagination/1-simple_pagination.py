6b53daaa96175d50489728e9adeeb9d3bbfafe0f
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-06 04:21:03 +0200

    Fix pycodestyle errors

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -10,9 +10,9 @@ def index_range(page, page_size):
     """
     A simple helper function to index pages
     """
-    assert isinstance(page, int) and page > 0,\
+    assert isinstance(page, int) and page > 0, \
         "Page must be a positive integer"
-    assert isinstance(page_size, int) and page_size > 0,\
+    assert isinstance(page_size, int) and page_size > 0, \
         "Page size must be a positive integer"
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
@@ -41,9 +41,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0,\
+        assert isinstance(page, int) and page > 0, \
             "Page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0,\
+        assert isinstance(page_size, int) and page_size > 0, \
             "Page size must be a positive integer"
 
         start, end = index_range(page, page_size)
