3799fef2ddc3d2702bbf3db37e6e4968b7b51a52
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-09 22:21:44 +0200

    use gettext function to parametrize your templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -36,7 +36,7 @@ def main():
     Renders the home page and returns the rendered template
     :return:
     """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
