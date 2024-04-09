0ba32ed494e345f0b040d2eff64a494f927f4fe6
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-09 16:36:53 +0200

    update app and instantiate the Babel object

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+A simple flask app that renders a template
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """
+    class Config to store languages and other data
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def main():
+    """
+    Renders the home page and returns the rendered template
+    :return:
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
