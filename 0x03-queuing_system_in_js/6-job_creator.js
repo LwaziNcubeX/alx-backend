d40890dd63a1194bbb2a936a4ef8af0b7a868260
Lwazi Ncube <lwazincubex@gmail.com>
2024-05-08 20:27:31 +0200

    change notification type

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -7,7 +7,7 @@ const jobData = {
 }
 
 const job = queue
-    .create('push_notification', jobData) // Change type to 'push_notification'
+    .create('push_notification_code', jobData)
     .save((err) => {
         if (!err) {
             console.log(`Notification job created: ${job.id}`);
