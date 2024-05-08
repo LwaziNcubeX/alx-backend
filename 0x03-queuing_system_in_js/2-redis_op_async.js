f896099aed8930ce2751c1cb96bda717b6db2cc3
Lwazi Ncube <lwazincubex@gmail.com>
2024-05-08 14:54:59 +0200

    Refactor Redis client implementation and add async functions

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+
+const { promisify } = require('util');
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+client.on('connect', function() {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', function(error) {
+    console.log(`Redis client not connected to the server: ${error}`);
+});
+
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+    try {
+        const value = await getAsync(schoolName);
+        console.log(value);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
