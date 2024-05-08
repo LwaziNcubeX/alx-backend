8a5e57bf5246582718d00643ac8cfe3b25284c80
Lwazi Ncube <lwazincubex@gmail.com>
2024-05-08 21:19:47 +0200

    Refactor job processor module for queuing system in JS

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -4,15 +4,15 @@ const queue = kue.createQueue();
 const blacklistNumbers = ['4153518780', '4153518781'];
 
 function sendNotification(phoneNumber, message, job, done) {
-    job.progress(0, 100); // Track the progress of the job
+    job.progress(0, 100);
     
     if (blacklistNumbers.includes(phoneNumber)) {
         const errorMessage = `Phone number ${phoneNumber} is blacklisted`;
         done(new Error(errorMessage));
     } else {
-        job.progress(50, 100); // Track the progress to 50%
+        job.progress(50, 100);
         console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-        done(); // Mark the job as done
+        done();
     }
 }
 
