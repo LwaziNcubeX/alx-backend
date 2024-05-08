56808e899a620951053fae1a41feee1b08879a92
Lwazi Ncube <lwazincubex@gmail.com>
2024-05-08 14:39:55 +0200

    Update Redis client import statement to use ES6 syntax

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,4 +1,4 @@
-const redis= require('redis');
+import redis from 'redis';
 const client = redis.createClient();
 client.on('connect', function() {
     console.log('Redis client connected to the server');
